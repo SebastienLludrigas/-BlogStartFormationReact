0ec61c4c06de538e5b3fd23d954bafcdaaac6a81
Sebastien Lludrigas <sebastienlludrigas@gmail.com>
2020-05-18 08:48:39 +0200

    10/51 fini

--- src/composants/Blog.js
+++ src/composants/Blog.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react'
 import NvPost from './NvPost/NvPost'
 import PostModale from './PostModale/PostModale'
 import './Blog.css'
+import axios from 'axios'
+import Post from './Post/Post'
+
 class Blog extends Component {
 
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(reponse => {
+                const articles = reponse.data.slice(0, 4);
+                const postAuteur = articles.map(post => {
+                    return {
+                        ...post,
+                        auteur: 'Hugo'
+                    }
+                })
+                this.setState({posts: postAuteur})
+            })
+    }
+
     render () {
+
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} auteur={post.auteur} titre={post.title}/>
+        })
 
         return (
             <div>
@@ -14,6 +39,7 @@ class Blog extends Component {
                 <h2 className="text-center my-5">Choisissez un post ...</h2>
                 <PostModale />
                 <section className="Posts">
+                    {posts}
                 </section>
 
             </div>
